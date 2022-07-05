53a5104cc6b13839419eed2df7bf4695b49e6955
Stephanos <stephanos@Stephanoss-MacBook-Pro.local>
2022-07-05 15:07:53 +0300

    relative paths

--- src/function-invocations/fetchSSO.js
+++ src/function-invocations/fetchSSO.js
@@ -1,6 +1,6 @@
 export const fetchSSO = async (accessToken) => {
   let res = await fetch(
-    `${process.env.REACT_APP_BASE_URL}/.netlify/functions/fetch-sso`,
+    `/.netlify/functions/fetch-sso`,
     {
       method: "GET",
       headers: { Authorization: `Bearer ${accessToken}` },
